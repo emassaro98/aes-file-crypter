41237b76652933cf0b0962593b1ecff0526d2111
Enrico Massaro <emassaro98@users.noreply.github.com>
2017-01-14 14:00:12 +0100

    First commit

---
+++ AesCrypter/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace AesCrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.labelFile = new System.Windows.Forms.Label();
+            this.labelStato = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOpen.ForeColor = System.Drawing.Color.Teal;
+            this.buttonOpen.Location = new System.Drawing.Point(168, 448);
+            this.buttonOpen.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(477, 207);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Encrypt";
+            this.buttonOpen.UseVisualStyleBackColor = false;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label1.Location = new System.Drawing.Point(360, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(617, 135);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select File";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonDecrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDecrypt.ForeColor = System.Drawing.Color.Teal;
+            this.buttonDecrypt.Location = new System.Drawing.Point(725, 448);
+            this.buttonDecrypt.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(477, 207);
+            this.buttonDecrypt.TabIndex = 2;
+            this.buttonDecrypt.Text = "Decrypt";
+            this.buttonDecrypt.UseVisualStyleBackColor = false;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // labelFile
+            // 
+            this.labelFile.AutoSize = true;
+            this.labelFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFile.ForeColor = System.Drawing.Color.SlateGray;
+            this.labelFile.Location = new System.Drawing.Point(32, 999);
+            this.labelFile.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.labelFile.Name = "labelFile";
+            this.labelFile.Size = new System.Drawing.Size(262, 38);
+            this.labelFile.TabIndex = 3;
+            this.labelFile.Text = "Selected file: null";
+            // 
+            // labelStato
+            // 
+            this.labelStato.AutoSize = true;
+            this.labelStato.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStato.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.labelStato.Location = new System.Drawing.Point(531, 725);
+            this.labelStato.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.labelStato.Name = "labelStato";
+            this.labelStato.Size = new System.Drawing.Size(287, 46);
+            this.labelStato.TabIndex = 4;
+            this.labelStato.Text = "Preparing file...";
+            this.labelStato.Visible = false;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Location = new System.Drawing.Point(496, 241);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(474, 38);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkGray;
+            this.label2.Location = new System.Drawing.Point(208, 243);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 38);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Insert password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.SlateGray;
+            this.label3.Location = new System.Drawing.Point(1057, 999);
+            this.label3.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(245, 38);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "By emassaro98";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1336, 1059);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.labelStato);
+            this.Controls.Add(this.labelFile);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonOpen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.Text = "AES 256 Crypting";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.Label labelFile;
+        private System.Windows.Forms.Label labelStato;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AesCrypter/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AesCrypter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static byte[] GenerateRandomSalt()
+        {
+            //Source: http://www.dotnetperls.com/rngcryptoserviceprovider
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                // Ten iterations.
+                for (int i = 0; i < 10; i++)
+                {
+                    // Fill buffer.
+                    rng.GetBytes(data);
+                }
+            }
+            return data;
+        }
+
+        //Encrypt
+        private void AES_Encrypt(string inputFile, string password)
+        {
+            //http://stackoverflow.com/questions/27645527/aes-encryption-on-large-files
+
+            //generate random salt
+            byte[] salt = GenerateRandomSalt();
+
+            //create output file name
+            FileStream fsCrypt = new FileStream(inputFile + ".encrypted", FileMode.Create);
+
+            //convert password string to byte arrray
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            //Set Rijndael symmetric encryption algorithm
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            //http://stackoverflow.com/questions/2659214/why-do-i-need-to-use-the-rfc2898derivebytes-class-in-net-instead-of-directly
+            //"What it does is repeatedly hash the user password along with the salt." High iteration counts.
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            //Cipher modes: http://security.stackexchange.com/questions/52665/which-is-the-best-cipher-mode-and-padding-mode-for-aes-encryption
+            AES.Mode = CipherMode.CFB;
+
+            //write salt to the begining of the output file, so in this case can be random every time
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents(); // -> for responsive GUI, using Task will be better!
+                    cs.Write(buffer, 0, read);
+                }
+
+                //close up
+                fsIn.Close();
+                labelStato.Text = "Successful! ";
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                labelStato.Text = "Error, try again! ";
+            }
+            finally
+            {
+                cs.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        //Decrypt
+        private void AES_Decrypt(string inputFile, string password)
+        {
+            //todo:
+            // - create error message on wrong password
+            // - on cancel: close and delete file
+            // - on wrong password: close and delete file!
+            // - create a better filen name
+            // - could be check md5 hash on the files but it make this slow
+
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CFB;
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(inputFile + ".decrypted", FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents();
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (System.Security.Cryptography.CryptographicException ex_CryptographicException)
+            {
+                Debug.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+                labelStato.Text = "Error, try again! ";
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+                labelStato.Text = "Error, try again! ";
+            }
+
+            try
+            {
+                labelStato.Text = "Successful! ";
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error by closing CryptoStream: " + ex.Message);
+                labelStato.Text = "Error, try again! ";
+            }
+            finally
+            {
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                labelFile.Text = "Selected file:" + openFileDialog1.FileName;
+                labelStato.Visible = true;
+                labelStato.Text = "Preparing file...";
+                if (txtPassword.Text != null & txtPassword.Text != " " & txtPassword.TextLength >= 8)
+                {
+                    AES_Encrypt(openFileDialog1.FileName, txtPassword.Text);
+
+                }
+                else {
+                    MessageBox.Show("Password not valid or not enough length! The min length must be 8 character", "Alert");
+                }
+                
+            }
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                labelFile.Text = "Selected file:" + openFileDialog1.FileName;
+                labelStato.Visible = true;
+                labelStato.Text = "Preparing file...";
+                if (txtPassword.Text != null & txtPassword.Text != " " & txtPassword.TextLength >= 8)
+                {
+                    AES_Decrypt(openFileDialog1.FileName, txtPassword.Text);
+
+                }
+                else
+                {
+                    MessageBox.Show("Password not valid or not enough length! The min length must be 8 character", "Alert");
+                }
+
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
